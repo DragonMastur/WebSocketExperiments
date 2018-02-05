2b780d77f46efac1b95b50c49ca0650906ec9319
Preston Hager <preston@hagerfamily.com>
2018-02-04 19:54:10 -0800

    Starting to implement websockets.

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "012345ABCDEF"
+socket = SocketIO(app)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return render_template("index.html")
+
+@app.route("/script/<script_name>")
+def script(src):
+    if src == "websockets":
+        return render_template("websockets.js")
+
+if __name__ == "__main__":
+    socket.run(app)
---
+++ run_app.bat
@@ -0,0 +1,3 @@
+cd C:\Users\discover\Documents\PrestonDeveloping\WebSocketExperiments\
+set FLASK_APP=flask_app.py
+python -m flask run
